ea0624a690efea95d2a75390c8f812995e440294
Vincent Matthys <vmatthys@student.42.fr>
2017-10-12 13:21:26 +0200

    Rendu du TP1

--- TP1.m
+++ TP1.m
@@ -29,7 +29,7 @@ R=sqrt(X.^2+Y.^2);
 M2=(acos(R)-R.*sqrt(1-min(R,1).^2)).*(R<=1);
 figure('Name', 'Fonction de transfert de modulation')
 subplot(211)
-Q=linspace(-10, 10, 1000)
+Q=linspace(-10, 10, 1000);
 plot(Q(501:1000), M2(501, 501:1000))
 title('Profil radial')
 subplot(212)
@@ -37,12 +37,36 @@ imshow(M2, 'XData', [-10, 10], 'YData', [-10, 10]);
 axis on
 title('Image bidimensionnelle')
 %%
-[X3,Y3]=meshgrid(linspace(-100,100,10000),linspace(-100,100,10000));
+[X3,Y3]=meshgrid(linspace(-10,10,1000),linspace(-10,10,1000));
 R3=sqrt(X3.^2+Y3.^2);
 M3=(2.*besselj(1,R3)./R3).^2;
 %%
-DFT=abs(fftshift(fft2(M3)));
+figure
+DFT=log(abs(fftshift(fft2(M3))));
 imshow(DFT)
+title('Log DFT tache d''Airy')
 % A=fftshift(fft2(M3));
 % B=sqrt(A.*conj(A));
 % imshow(B)
+
+%%
+%Question 6
+[X6,Y6]=meshgrid(linspace(-10,10,1000),linspace(-10,10,1000));
+R6=sqrt(X6.^2+Y6.^2);
+M6=(2.*besselj(1,R6)./R6 - 2.*besselj(1, R6.*0.25)./(R6.*0.25)).^2;
+figure('Name', 'Tache du telescope')
+subplot(211)
+imshow(M6);
+title('Sans saturation')
+subplot(212), imshow(min(100 * M6, ones(size(M))))
+title('Avec saturation à 0.01')
+%%
+figure
+DFT6=log(abs(fftshift(fft2(M6))));
+figure('Name', 'Comparaison télescope et lunette')
+subplot(211)
+imshow(DFT)
+title('FTM lunette')
+subplot(212)
+imshow(DFT6)
+title('FTM télescope')

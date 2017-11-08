9c449b0360d3fcee36b5cf162c95b3dbc00e3969
Vincent Matthys <vmatthys@student.42.fr>
2017-11-09 00:28:50 +0100

    Exercise 8.1

--- TP3/TP3.m
+++ TP3/TP3.m
@@ -17,11 +17,30 @@ axis on;
 imshow(normsat(abs(f),1));
 %%
 imshow(normsat(fftshift(abs(f)),1));
+%% Visualistions efficaces :
+% de la partie réelle
+imshow(log(1 +abs(real(fftshift(f)))), [])
+% de la partie imaginaire
+imshow(log(1 +abs(imag(fftshift(f)))), [])
+% de la phase
+imshow(angle(fftshift(f)), [])
+% du module
+imshow(log(1 + abs(fftshift(f))), [])
 
 %% Exercice 8 : Periodisation implicite de la transformee de Fourier discrete
 u = double(imread('lena.pgm'));
 imshow(u,[]);
 v = fshift(u,-30,-30);
 figure();imshow(v,[]);
-v = fshift(u,-300,-300);
-figure();imshow(v,[]);
+% v = fshift(u,-300,-300);
+% figure();imshow(v,[]);
+%% Transformée de Fourier en module
+% de u
+imshow(log(1 + abs(fftshift(fft2(u)))), [])
+% de v
+figure ; imshow(log(1 + abs(fftshift(fft2(v)))), [])
+%% Transformée de Fourier en phase
+% de u
+imshow(angle(fftshift(fft2(u))), [])
+% de v
+figure ; imshow(angle(fftshift(fft2(v))), [])

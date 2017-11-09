1ee38b5171712614b808097dd95744db729200a3
Vincent Matthys <vmatthys@student.42.fr>
2017-11-09 10:00:31 +0100

    Phase exchange

--- TP3/exchange_phase.m
+++ TP3/exchange_phase.m
@@ -1,10 +1,14 @@
 function [U,V] = exchange_phase(u,v)
 %exchange_phase Exchange phase between two images
 
-A = fft2(u);
-B = fft2(v);
+if size(u) ~= size(v)
+    U = u;
+    V = v;
+else
+    A = fft2(u);
+    B = fft2(v);
 
-U = real(ifft(exp(i*angle(B)).*abs(A)));
-V = real(ifft(exp(i*angle(A)).*abs(B)));
+    U = real(ifft2(exp(j*angle(A)).*abs(B)));
+    V = real(ifft2(exp(j*angle(B)).*abs(A)));
 end
 

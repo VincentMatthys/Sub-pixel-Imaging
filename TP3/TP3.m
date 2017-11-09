08aa8932421ada3884121b195e8dd3b0631f4862
Vincent Matthys <vmatthys@student.42.fr>
2017-11-09 11:14:40 +0100

    Exercise 10 question 2 wi

--- TP3/TP3.m
+++ TP3/TP3.m
@@ -1,3 +1,4 @@
+addpath('images/')
 addpath('../images/')
 addpath('../iptools/')
 
@@ -50,10 +51,95 @@ figure ; imshow(angle(fftshift(fft2(v))), [])
 % -------------------------------------------------------------------------
 %% Exercice 10 : Synthèse de microtextures
 % -------------------------------------------------------------------------
-a = double(imread('bouc.pgm'));
-b = double(imread('baobab2.pgm'));
+%% Question 1 : échange de phase
+a = double(imread('room.pgm'));
+b = double(imread('saturne.pgm'));
 imshow(b, [])
 
 [c,d] = exchange_phase(a, b);
 
-imshow(d, []);
+imshow([a,b], []);
+figure ; imshow([c,d], []);
+
+%% Autre couple d'image pour échange de phase
+a2 = double(imread('room.pgm'));
+b2 = double(imread('lena.pgm'));
+imshow(b, [])
+
+[c2,d2] = exchange_phase(a2, b2);
+
+imshow([a2,b2], []);
+figure ; imshow([c2,d2], []);
+
+%% Autre couple d'image pour échange de phase
+a2 = imread('cameraman.jpg');
+b2 = imread('47.gif');
+imshow(b, [])
+
+[c2,d2] = exchange_phase(a2, b2);
+
+imshow([a2,b2], []);
+figure ; imshow([c2,d2], []);
+
+%% Question 2 : phase aléatoire
+u = double(imread('lena.pgm'));
+% imshow(u, []);
+p = perdecomp(u);
+r = randphase(p);
+imshow([u, r], []);
+
+%% Ecorce
+e = double(imread('Bark.tiff'));
+pe = perdecomp(e);
+re = randphase(pe);
+imshow([e, re], []);
+
+%% Paille
+e = double(imread('Straw.tiff'));
+pe = perdecomp(e);
+re = randphase(pe);
+imshow([e, re], []);
+%% Briques
+e = double(imread('Brick_wall.tiff'));
+pe = perdecomp(e);
+re = randphase(pe);
+imshow([e, re], []);
+%% Bulles plastiques
+e = double(imread('Plastic_bubbles.tiff'));
+pe = perdecomp(e);
+re = randphase(pe);
+imshow([e, re], []);
+
+%% Tissu écossais => marche bien
+e = double(imread('Weave.tiff'));
+pe = perdecomp(e);
+re = randphase(pe);
+imshow([e, re], []);
+
+%% Sable
+e = double(imread('Beach_sand.tiff'));
+pe = perdecomp(e);
+re = randphase(pe);
+imshow([e, re], []);
+
+%% Raffia
+e = double(imread('raffia.tiff'));
+pe = perdecomp(e);
+re = randphase(pe);
+imshow([e, re], []);
+%% noise
+e = double(imread('noise.png'));
+pe = perdecomp(e);
+re = randphase(pe);
+imshow([e, re], []);
+%% image de départ noise
+e = double(imread('noise.png'));
+% pe = perdecomp(e);
+re = randphase(e);
+imshow([e, re], []);
+
+%% image de départ lena
+e = double(imread('Weave.tiff'));
+% pe = perdecomp(e);
+re = randphase(e);
+imshow([e, re], []);
